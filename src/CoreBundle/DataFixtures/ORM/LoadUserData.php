02cb780e52aba8098d103fe57e7595d47bf09149
Lukasz Malicki <maalicki@gmail.com>
2016-12-10 21:38:22 +0100

    added fixtures birthdate

--- src/CoreBundle/DataFixtures/ORM/LoadUserData.php
+++ src/CoreBundle/DataFixtures/ORM/LoadUserData.php
@@ -8,6 +8,7 @@
 
 namespace CoreBundle\DataFixtures\ORM;
 
+use CoreBundle\Entity\User;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
@@ -36,6 +37,7 @@ class LoadUserData implements FixtureInterface, ContainerAwareInterface, Ordered
 
         foreach ($this->userList() as $userArr) {
 
+            /** @var $user User */
             $user = $userManager->createUser();
 
             $user
@@ -43,6 +45,7 @@ class LoadUserData implements FixtureInterface, ContainerAwareInterface, Ordered
                 ->setEmail(sprintf('%s@localhost.net', $userArr['username']))
                 ->setRoles($userArr['roles'])
                 ->setFirstName($userArr['firstName'])
+                ->setBirthDate((new \DateTime($userArr['birthDate'])))
                 ->setLastName($userArr['lastName'])
                 ->setGender($userArr['gender'])
                 ->setEnabled($userArr['enabled']);
@@ -60,16 +63,22 @@ class LoadUserData implements FixtureInterface, ContainerAwareInterface, Ordered
     public function userList()
     {
         $list = [
-            ['username' => 'root', 'roles' => ['ROLE_ADMIN', 'ROLE_API'], 'password' => 'root', 'enabled' => true,
-                'firstName' => 'James T.', 'lastName' => 'Kirk', 'gender' => 'male'],
-            ['username' => 'user1', 'roles' => ['ROLE_USER', 'ROLE_API'], 'password' => 'user1', 'enabled' => true,
-                'firstName' => 'Deanna', 'lastName' => 'Troi', 'gender' => 'female'],
-            ['username' => 'user2', 'roles' => ['ROLE_USER', 'ROLE_API'], 'password' => 'user2', 'enabled' => true,
-                'firstName' => 'Nyota', 'lastName' => 'Uhura', 'gender' => 'female'],
-            ['username' => 'user3', 'roles' => ['ROLE_USER', 'ROLE_API'], 'password' => 'user3', 'enabled' => true,
-                'firstName' => 'Hikrau', 'lastName' => 'Sulu', 'gender' => 'male'],
-            ['username' => 'user4', 'roles' => ['ROLE_USER', 'ROLE_API'], 'password' => 'user4', 'enabled' => false,
-                'firstName' => 'Montgomery', 'lastName' => 'Scott', 'gender' => 'male'],
+            [
+                'username' => 'root', 'roles' => ['ROLE_ADMIN'], 'password' => 'root', 'enabled' => true,
+                'firstName' => 'James T.', 'lastName' => 'Kirk', 'gender' => 'male', 'birthDate' => '1970-01-01'
+            ],
+            ['username' => 'user1', 'roles' => ['ROLE_USER'], 'password' => 'user1', 'enabled' => true,
+                'firstName' => 'Deanna', 'lastName' => 'Troi', 'gender' => 'female', 'birthDate' => '2005-01-01'
+            ],
+            ['username' => 'user2', 'roles' => ['ROLE_USER'], 'password' => 'user2', 'enabled' => true,
+                'firstName' => 'Nyota', 'lastName' => 'Uhura', 'gender' => 'female', 'birthDate' => '1987-01-01'
+            ],
+            ['username' => 'user3', 'roles' => ['ROLE_USER'], 'password' => 'user3', 'enabled' => true,
+                'firstName' => 'Hikrau', 'lastName' => 'Sulu', 'gender' => 'male', 'birthDate' => '1990-01-01'
+            ],
+            ['username' => 'user4', 'roles' => ['ROLE_USER'], 'password' => 'user4', 'enabled' => false,
+                'firstName' => 'Montgomery', 'lastName' => 'Scott', 'gender' => 'male', 'birthDate' => '1999-01-01'
+            ],
         ];
 
         return $list;
